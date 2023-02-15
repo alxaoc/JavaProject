1dc881c1f877f17ad54069eb95c0006607010bab
MoToR <alex.bublik.82@gmail.com>
2023-02-15 18:48:00 +0200

    Added io.github.bonigarcia New page Homework_02 Push

---
+++ src/main/java/Homework_02.java
@@ -0,0 +1,8 @@
+public class Homework_02 {
+
+    public static void main(String[] args) {
+
+
+        System.out.printf("In process...");
+    }
+}
--- src/test/java/TestLesson1.java
+++ src/test/java/TestLesson1.java
@@ -1,6 +1,33 @@
 public class TestLesson1 {
     public static void main(String[] args) {
 
-        System.out.print("Lesson # 1");
+        String proProcess = "«Виробничий процес WPT»";
+        System.out.println(proProcess);
+
+        int engineer1 = 8;
+        int engineer2 = 12;
+        int engineer3 = 4;
+
+        int totalDays = engineer1 + engineer2 + engineer3; //Загальна кількість днів
+        System.out.println("Загальна кількість днів: " + totalDays + ".");
+
+        // Середня продуктивність праці = загальна кількість регресійних тестів / загальна кількість робочих днів.
+
+        int totalTests = 1; // загальна кількість регресійних тестів
+        System.out.println("Загальна кількість тестів: " + totalTests + ".");
+
+        double averageProductivity = (double) totalTests / totalDays;
+        System.out.println("Середня продуктивність праці: " + averageProductivity + " тестів на день.");
+
+        double avePro1 = (double) engineer1 / totalDays * 100;   // Середня продуктивність праці 1 інженера
+        System.out.println("Середня продуктивність праці 1 інженера: " + avePro1 + "% відсртків від загальної кількості днів.");
+
+        double avePro2 = (double) engineer2 / totalDays * 100;   // Середня продуктивність праці 2 інженера
+        System.out.println("Середня продуктивність праці 2 інженера: " + avePro2 + "% відсртків від загальної кількості днів.");
+
+        double avePro3 = (double) engineer3 / totalDays * 100;   // Середня продуктивність праці 3 інженера
+        System.out.println("Середня продуктивність праці 3 інженера: " + avePro3 + "% відсртків від загальної кількості днів.");
+
+        System.out.println("Всім дякую!!! Гароного дня ;) ");
     }
 }
