aebd5188fae5d2cd275425c5e8aaf59e6a0432ab
alxaoc <alex.bublik.82@gmail.com>
2023-02-17 22:25:07 +0200

    Lesson_2

--- src/main/java/Homework_02.java
+++ src/main/java/Homework_02.java
@@ -3,7 +3,7 @@ public class Homework_02 {
     public static void main(String[] args) {
 
 
-        System.out.printf("In process...");
-        System.out.printf("In process...");
+        System.out.print("In process...");
+        System.out.print("In process...");
     }
 }
